a86dfeeb956d8b83419a79b9bf760253cab0e0e0
Kenny Gwon <kgwon1@swarthmore.edu>
2018-03-30 11:38:47 -0400

    stats for 90-92

--- checkStats.py
+++ checkStats.py
@@ -2,28 +2,31 @@ import json
 
 def main():
 	
-	season = input("season: ")
-	season = "2013-14"
+	season = str(input("season: "))
+
 	seasonTextFile = season + ".txt"
 	with open(seasonTextFile) as seasonJSONfile:
 		currentDictionary = json.load(seasonJSONfile)
 	running = True
-	while running:
-		print("Print 'g' to look up stats for a single game")
-		if input("'Quit' to quit") == "quit":
-			break
-		else:
-			year = input("year: ")
-			month = input("month: ")
-			day = input("day: ")
-			date = year + "-" + month + "-" + day
+	#while running:
+        team = input("team: ")
+        print(len(currentDictionary.get(team)))
+
+                #print("Print 'g' to look up stats for a single game")
+		#if input("'Quit' to quit") == "quit":
+		#	break
+		#else:
+		#	year = input("year: ")
+		#	month = input("month: ")
+		#	day = input("day: ")
+		#	date = year + "-" + month + "-" + day
 			
-			team = input("team: ")
+		#	team = input("team: ")
 
 
 
-			for game in currentDictionary.get(team):
-				if game.get("date") == date:
-					print(game)
+		#	for game in currentDictionary.get(team):
+		#		if game.get("date") == date:
+		#			print(game)
 
 main()
