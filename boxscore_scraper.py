693f71313ce46d35b2cc3de62a446fdd9bffa565
Kenny <kennygwon@gmail.com>
2018-04-01 21:40:51 -0400

    starting from '13

--- boxscore_scraper.py
+++ boxscore_scraper.py
@@ -219,7 +219,7 @@ def main():
 	today = datetime.date.today()
 	lastDate = datetime.date.today()
 	
-	firstDate = datetime.date(2007, 7, 16)
+	firstDate = datetime.date(2013, 7, 16)
 	lastDate = datetime.date(2017, 7, 15)
 
 	# lastDate = datetime.date(today.year, 3, 20)
